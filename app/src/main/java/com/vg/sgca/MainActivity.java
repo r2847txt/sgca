2a0f1a1f908a6f6e95acceca69b47854e86aad9d
Rodrigo Saavedra <rdrg.saavedra@gmail.com>
2023-11-02 20:17:34 -0300

    comentario de prueba

--- app/src/main/java/com/vg/sgca/MainActivity.java
+++ app/src/main/java/com/vg/sgca/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // prueba
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
