8472609206edb402e17b961f9f6a7055dfb7a53a
r2847txt <r2847txt@gmail.com>
2023-11-22 00:04:48 -0300

    Registro exitoso (sin cambio)

--- app/src/main/java/com/vg/sgca/RegisterActivity.java
+++ app/src/main/java/com/vg/sgca/RegisterActivity.java
@@ -3,67 +3,91 @@ package com.vg.sgca;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
-    TextInputEditText et_nombre, et_apellido, et_rut, et_correo, et_pass1, et_pass2;
-    Button bt_registrarse;
+    EditText et_nombre, et_correo, et_contrasena;
+    Button bt_registrar;
+    FirebaseFirestore mFirestore;
     FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFirestore = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
+
         et_nombre = findViewById(R.id.et_nombre);
-        et_apellido = findViewById(R.id.et_apellido);
-        et_rut = findViewById(R.id.et_rut);
         et_correo = findViewById(R.id.et_correo);
-        et_pass1 = findViewById(R.id.et_pass1);
-        et_pass2 = findViewById(R.id.et_pass2);
-        bt_registrarse = findViewById(R.id.bt_registrarse);
+        et_contrasena = findViewById(R.id.et_contrasena);
+        bt_registrar = findViewById(R.id.bt_registrar);
 
-        bt_registrarse.setOnClickListener(new View.OnClickListener() {
+        bt_registrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String correo, pass1;
-                correo = String.valueOf(et_correo.getText());
-                pass1 = String.valueOf(et_pass1.getText());
+            public void onClick(View v) {
+                String nombreUsuario = et_nombre.getText().toString().trim();
+                String correoUsuario = et_correo.getText().toString().trim();
+                String contrasenaUsuario = et_contrasena.getText().toString().trim();
 
-                if(TextUtils.isEmpty(correo)) {
-                    Toast.makeText(RegisterActivity.this, "Ingrese email", Toast.LENGTH_SHORT).show();
-                    return;
+                if(nombreUsuario.isEmpty() && correoUsuario.isEmpty() && contrasenaUsuario.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Debe ingresar los datos solicitados", Toast.LENGTH_SHORT).show();
+                }else{
+                    registrarUsuario(nombreUsuario, correoUsuario, contrasenaUsuario);
                 }
 
-                if(TextUtils.isEmpty(pass1)) {
-                    Toast.makeText(RegisterActivity.this, "Ingrese clave", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+            }
+        });
+    }
 
-                mAuth.createUserWithEmailAndPassword(correo, pass1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(RegisterActivity.this, "Cuenta creada.",
-                                            Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(RegisterActivity.this, "No se pudo crear la cuenta.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
+    private void registrarUsuario(String nombreUsuario, String correoUsuario, String contrasenaUsuario) {
+        mAuth.createUserWithEmailAndPassword(correoUsuario, contrasenaUsuario).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                String id = mAuth.getCurrentUser().getUid();
+
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", id);
+                map.put("nombre", nombreUsuario);
+                map.put("correo", correoUsuario);
+                map.put("contraseña", contrasenaUsuario);
+
+                mFirestore.collection("usuario").document(id).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                        Toast.makeText(RegisterActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegisterActivity.this, "Error al guardar", Toast.LENGTH_SHORT).show();
+                    }
                 });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this, "Error al registrar", Toast.LENGTH_SHORT).show();
             }
         });
     }
