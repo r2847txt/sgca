1737f018386c8ba600a194cbb6d6f970c4ca6378
r2847txt <r2847txt@gmail.com>
2023-11-23 21:00:09 -0300

    Se hizo lo que se pudo

--- app/src/main/java/com/vg/sgca/CitasCrearActivity.java
+++ app/src/main/java/com/vg/sgca/CitasCrearActivity.java
@@ -4,19 +4,73 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.Calendar;
+
 public class CitasCrearActivity extends AppCompatActivity {
+    EditText et_asuntoCita, et_detalleCita;
+    TextView tv_fechaSeleccionada;
+    Button bt_seleccionarFecha;
+    int dia, mes, ano;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citas_crear);
+        et_asuntoCita = findViewById(R.id.et_asuntoCita);
+        et_detalleCita = findViewById(R.id.et_detalleCita);
+        tv_fechaSeleccionada = findViewById(R.id.tv_fechaSeleccionada);
+        bt_seleccionarFecha = findViewById(R.id.bt_seleccionarFecha);
 
+        bt_seleccionarFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendario = Calendar.getInstance();
+
+                dia = calendario.get(Calendar.DAY_OF_MONTH);
+                mes = calendario.get(Calendar.MONTH);
+                ano = calendario.get(Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(CitasCrearActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int anoSeleccionado, int mesSeleccionado, int diaSeleccionado) {
+                        String formatoDia, formatoMes;
+                        int mes = mesSeleccionado + 1;
+
+                        if (diaSeleccionado < 10) {
+                            formatoDia = "0" + String.valueOf(diaSeleccionado);
+                        } else {
+                            formatoDia = String.valueOf(diaSeleccionado);
+                        }
+
+                        if (mes < 10) {
+                            formatoMes = "0" + String.valueOf(mes);
+                        }  else {
+                            formatoMes = String.valueOf(mes);
+                        }
+
+                        tv_fechaSeleccionada.setText(formatoDia + "/" + formatoMes + "/" + anoSeleccionado);
+
+                    }
+                }, ano, mes,dia);
+                datePickerDialog.show();
+            }
+        });
+
+
+        //NAVEGACION
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Detalle de cita");
